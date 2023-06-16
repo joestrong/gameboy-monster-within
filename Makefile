f39dcbedbb66e74f584294fb7661d3bfaf0201a2
Joe Strong <joestrong@gmail.com>
2023-06-16 22:34:42 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,23 @@
+.PHONY: gfx
+
+CC	= ${GBDK_HOME}/bin/lcc -Wa-l -Wl-m -Wl-j
+
+all: gfx main
+
+main: src/main.c
+	$(CC) -o out/main.gb \
+		src/main.c \
+		src/tiles/gbcompologo.c \
+		src/tiles/title.c \
+		src/tiles/overworld.c \
+		src/tiles/sprites.c
+
+gfx: gfx/*.png
+	png2asset gfx/gbcompologo.png -c src/tiles/gbcompologo.c -map
+	png2asset gfx/title.png -c src/tiles/title.c -map
+	png2asset gfx/overworld.png -c src/tiles/overworld.c -map
+	png2asset gfx/sprites.png -c src/tiles/sprites.c -spr8x8 -sw 16 -sh 16
+
+clean:
+	rm -f *.o *.lst *.map *.gb *~ *.rel *.cdb *.ihx *.lnk *.sym *.asm *.noi
+
---
+++ src/main.c
@@ -0,0 +1,237 @@
+#include <stdint.h>
+#include <gb/gb.h>
+#include "./tiles/gbcompologo.h"
+#include "./tiles/title.h"
+#include "./tiles/overworld.h"
+#include "./tiles/sprites.h"
+
+#define skip_intro 0
+
+#define MIN(A,B) ((A)<(B)?(A):(B))
+#define bigmap_mapHeight 32
+#define bigmap_mapWidth 32
+
+void loadCompoLogoScreen();
+void updateCompoLogoScreen();
+void loadTitleScreen();
+void updateTitleScreen();
+void loadGame();
+void updateGame();
+void set_camera();
+
+uint8_t state = 0; // 0 - Compo Logo 1 - Title, 2 - Game
+uint8_t counter = 0;
+
+int16_t camera_x = 0;
+int16_t camera_y = 0;
+int16_t old_camera_x = 0;
+int16_t old_camera_y = 0;
+int8_t map_pos_x = 0;
+int8_t map_pos_y = 0;
+int8_t old_map_pos_x = 0;
+int8_t old_map_pos_y = 0;
+uint8_t player_x = 72;
+uint8_t player_y = 80;
+
+void main() {
+  DISPLAY_OFF;
+  LCDC_REG = LCDCF_OFF | LCDCF_BG8800 | LCDCF_BG9800 | LCDCF_BGON | LCDCF_OBJON;
+
+  // Set pallette defaults
+  BGP_REG = OBP0_REG = OBP1_REG = 0xE4U;
+
+  #if skip_intro == 1
+    state = 2;
+    loadGame();
+  #else
+    loadCompoLogoScreen();
+  #endif
+
+  while (1) {
+    wait_vbl_done();
+
+    if (state == 0) {
+      updateCompoLogoScreen();
+    }
+
+    if (state == 1) {
+      updateTitleScreen();
+    }
+
+    if (state == 2) {
+      updateGame();
+    }
+  }
+}
+
+void loadCompoLogoScreen() {
+  set_bkg_data(0x00, gbcompologo_TILE_COUNT, gbcompologo_tiles);
+  set_bkg_tiles(0, 0, 20, 18, gbcompologo_map);
+  counter = 0;
+  BGP_REG = 0x00;
+   
+  DISPLAY_ON;
+}
+
+void updateCompoLogoScreen() {
+  switch (counter) {
+    case 5:
+      BGP_REG = 0x04;
+      break;
+    case 10:
+      BGP_REG = 0x24;
+      break;
+    case 15:
+      BGP_REG = 0xE4;
+      break;
+    case 120:
+      BGP_REG = 0x24;
+      break;
+    case 125:
+      BGP_REG = 0x04;
+      break;
+    case 130:
+      BGP_REG = 0x00;
+      break;
+    case 135:
+      state = 1;
+      loadTitleScreen();
+      break;
+  }
+  counter++;
+}
+
+void loadTitleScreen() {
+  DISPLAY_OFF;
+
+  set_bkg_data(0x00, title_TILE_COUNT, title_tiles);
+  set_bkg_tiles(0, 0, 20, 18, title_map);
+  counter = 0;
+   
+  DISPLAY_ON;
+}
+
+void updateTitleScreen() {
+  switch (counter) {
+    case 5:
+      BGP_REG = 0x04;
+      break;
+    case 10:
+      BGP_REG = 0x24;
+      break;
+    case 15:
+      BGP_REG = 0xE4;
+      break;
+    case 160:
+      BGP_REG = 0x24;
+      break;
+    case 165:
+      BGP_REG = 0x04;
+      break;
+    case 170:
+      BGP_REG = 0x00;
+      break;
+    case 175:
+      state = 2;
+      loadGame();
+      break;
+  }
+  counter++;
+}
+
+void loadGame() {
+  DISPLAY_OFF;
+
+  set_bkg_data(0x00, overworld_TILE_COUNT, overworld_tiles);
+  set_bkg_submap(0, 0, 20, 18, overworld_map, bigmap_mapWidth);
+  counter = 0;
+
+  set_sprite_data(0x00, sprites_TILE_COUNT, sprites_tiles);
+  set_sprite_tile(0, 0);
+  set_sprite_tile(1, 1);
+  set_sprite_tile(2, 2);
+  set_sprite_tile(3, 0);
+  set_sprite_tile(4, 1);
+  set_sprite_tile(5, 2);
+  set_sprite_prop(3, S_FLIPX);
+  set_sprite_prop(4, S_FLIPX);
+  set_sprite_prop(5, S_FLIPX);
+  move_sprite(0, player_x, player_y);
+  move_sprite(1, player_x, player_y + 8);
+  move_sprite(2, player_x, player_y + 16);
+  move_sprite(3, player_x + 8, player_y);
+  move_sprite(4, player_x + 8, player_y + 8);
+  move_sprite(5, player_x + 8, player_y + 16);
+
+  DISPLAY_ON;
+}
+
+void updateGame() {
+  switch (counter) {
+    case 5:
+      BGP_REG = 0x04;
+      break;
+    case 10:
+      BGP_REG = 0x24;
+      break;
+    case 15:
+      BGP_REG = 0xE4;
+      break;
+  }
+  if (counter < 15) {
+    counter++;
+  }
+
+  int keys = joypad();
+  if (keys & J_UP) {
+    camera_y--;
+  }
+  if (keys & J_DOWN) {
+    camera_y++;
+  }
+  if (keys & J_LEFT) {
+    camera_x--;
+  }
+  if (keys & J_RIGHT) {
+    camera_x++;
+  }
+
+  if (camera_x < 0) {
+    camera_x = 0;
+  }
+  if (camera_y < 0) {
+    camera_y = 0;
+  }
+  
+  set_camera();
+}
+
+void set_camera() {
+    SCY_REG = camera_y;
+    SCX_REG = camera_x; 
+    
+    map_pos_y = (uint8_t)(camera_y >> 3u);
+    if (map_pos_y != old_map_pos_y) { 
+        if (camera_y < old_camera_y) {
+            set_bkg_submap(map_pos_x, map_pos_y, MIN(21u, bigmap_mapWidth-map_pos_x), 1, overworld_map, bigmap_mapWidth);
+        } else {
+            if ((bigmap_mapHeight - 18u) > map_pos_y) {
+              set_bkg_submap(map_pos_x, map_pos_y + 18u, MIN(21u, bigmap_mapWidth-map_pos_x), 1, overworld_map, bigmap_mapWidth);
+            }
+        }
+        old_map_pos_y = map_pos_y; 
+    }
+    
+    map_pos_x = (uint8_t)(camera_x >> 3u);
+    if (map_pos_x != old_map_pos_x) {
+        if (camera_x < old_camera_x) {
+            set_bkg_submap(map_pos_x, map_pos_y, 1, MIN(19u, bigmap_mapHeight - map_pos_y), overworld_map, bigmap_mapWidth);     
+        } else {
+            if ((bigmap_mapWidth - 20u) > map_pos_x) set_bkg_submap(map_pos_x + 20u, map_pos_y, 1, MIN(19u, bigmap_mapHeight - map_pos_y), overworld_map, bigmap_mapWidth);     
+        }
+        old_map_pos_x = map_pos_x;
+    }
+    
+    old_camera_x = camera_x;
+    old_camera_y = camera_y;
+}
